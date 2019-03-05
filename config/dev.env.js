6d1a3fc30cf92910934d9dece6db75285385c83a
fanxinglong <fxllong262553@gmail.com>
2019-03-05 14:30:51 +0800

    端口修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:2004"',
+  BASE_API: '"http://localhost:2019"',
 })
